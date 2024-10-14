72e906b1afcd87bbbf105db6550e053b3ccb66f1
Kavya <kavyaks0709@gmail.com>
2024-10-14 22:40:43 +0530

    edit in css

--- backend/routes/statusRoutes.js
+++ backend/routes/statusRoutes.js
@@ -5,8 +5,9 @@ const router = express.Router();
 const OrderStatus = require('../models/Status.js');
 
 // Update order status by orderId and stage
-router.put('/:orderId/stage/:stageIndex', async (req, res) => {
-    const { orderId, stageIndex } = req.params;
+// Fetch order status by orderId
+router.get('/:orderId', async (req, res) => {
+    const { orderId } = req.params;
 
     try {
         const order = await OrderStatus.findOne({ orderId });
@@ -15,29 +16,12 @@ router.put('/:orderId/stage/:stageIndex', async (req, res) => {
             return res.status(404).json({ message: 'Order not found' });
         }
 
-        const stage = order.status[stageIndex];
-
-        if (stage) {
-            if (req.body.completed === true && stage.completed === false) {
-                // Set the current timestamp when completed becomes true
-                stage.timestamp = new Date(); 
-            }
-
-            // Update the completed status
-            stage.completed = req.body.completed;
-
-            // Save the updated order status
-            await order.save();
-
-            res.json(order);
-        } else {
-            res.status(404).json({ message: 'Stage not found' });
-        }
-
+        res.json(order);
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Server error' });
     }
 });
+
 
 module.exports = router;
